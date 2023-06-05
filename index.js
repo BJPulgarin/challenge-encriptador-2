4dad395dd088543ac9fcff2008ed5446be279793
Brajan Pulgarin <bjpulgarin@gmail.com>
2023-06-05 12:30:53 -0500

    página funcional con responsive

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const solution = document.querySelector(".textResult")
 const searching = document.querySelector(".searching")
 const p1_2 = document.querySelector(".p1-2")
 const result = document.querySelector(".result")
+const x = document.querySelector(".x")
+const pattern = new RegExp(/^[A-Za-z0-9\.,\s]+$/g)
+x.style.display = "none"
 
 solution.style.display = "none"
 let aux = true
@@ -12,47 +15,59 @@ let aux = true
 function encriptar(event) {
   event.preventDefault();
   if (input.value == ""){
-    alert("¡ESCRIBE UN TEXTO!")
+    alert("¡Escribe un texto!")
   }
   else{
-    let text = input.value.split("");
-    for(let i=0; i<text.length; i++){
-        switch (text[i]){
-            case "a" : text[i] = "ai";
-            break;
-            case "e" : text[i] = "enter";
-            break;
-            case "i" : text[i] = "imes";
-            break;
-            case "o" : text[i] = "ober";
-            break;
-            case "u" : text[i] = "ufat";
-            break;
-        }
+    let isValid = false
+    if(!pattern.test(input.value)){
+      isValid = false
+    }else{
+      isValid = true
     }
-    let encText = text.join("")
-    solution.textContent = encText;
-    searching.style.display = "none"
-    p1_2.style.display = "none"
-    solution.style.display = "block"
-    result.style.display = "block"
-    if (aux){
-        crearCopy()
+    console.log("esto funciona")
+    if (isValid==true){
+      let text = input.value.split("");
+      for(let i=0; i<text.length; i++){
+          switch (text[i]){
+              case "a" : text[i] = "ai";
+              break;
+              case "e" : text[i] = "enter";
+              break;
+              case "i" : text[i] = "imes";
+              break;
+              case "o" : text[i] = "ober";
+              break;
+              case "u" : text[i] = "ufat";
+              break;
+          }
+      }
+      let encText = text.join("")
+      solution.textContent = encText;
+      searching.style.display = "none"
+      p1_2.style.display = "none"
+      solution.style.display = "block"
+      result.style.display = "block"
+      if (aux){
+          crearCopy()
+      }
+      aux = false
+      copy.addEventListener("click", function(event){
+          event.preventDefault()
+          navigator.clipboard.writeText(encText)
+          copy.textContent = "¡Texto copiado!"
+      })
+      copy.textContent = "Copia tu texto"
     }
-    aux = false
-    copy.addEventListener("click", function(event){
-        event.preventDefault()
-        navigator.clipboard.writeText(encText)
-        copy.textContent = "¡Texto copiado!"
-    })
-    copy.textContent = "Copia tu texto"
+    else{
+      alert("El texto ingresado no es válido")
+    }
   }
 }
 
 function desencriptar (event){
   event.preventDefault()
   if (input.value == ""){
-    alert("¡ESCRIBE UN TEXTO!")
+    alert("¡Escribe un texto!")
   }
   else{
     let text = input.value
@@ -79,7 +94,6 @@ function desencriptar (event){
   }
 }
 
-
 function crearCopy(){
     let copy = document.createElement("button")
     copy.setAttribute("id", "copy")
@@ -88,6 +102,22 @@ function crearCopy(){
     return copy
 }
 
+function delete_text (){
+  input.value=""
+  x.style.display = "none"
+}
+
+function minus(){
+  input.value = input.value.toLowerCase();
+  if (input.value != ""){
+    x.style.display = "block"
+  }
+  else{
+    x.style.display = "none"
+  }
+}
 
 encriptBtn.onclick = encriptar;
 desencriptarBtn.onclick = desencriptar;
+x.onclick = delete_text;
+input.onkeyup = minus;
